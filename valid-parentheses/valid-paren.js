ec2ccdf439fc1af8b80be7789b358f944ff54b2e
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-07-08 15:19:25 -0700

    improved time by deleting split operation

--- valid-parentheses/valid-paren.js
+++ valid-parentheses/valid-paren.js
@@ -3,47 +3,44 @@
 
 // look for the first parenthesis and then the second parenthasis. and then check within these parenthasis
 var isValid = function(s) {
-  // IDEA: iterate through the string and use stack to make sure that a closing paren is        matched to the closest open paren
-  // make stack arr
-  let stack = []
-  
-  // define open/close obj
-  let open = {
-      '(': ')',
-      '[': ']',
-      '{': '}',
-  };
-  let close = {
-      ')': '(',
-      ']': '[',
-      '}': '{',
-  };
-  
-  // split string to arr
-  let arr = s.split('')
-  
-  // iterate throug arr
-  for (let i = 0; i < arr.length; i++) {
-      // if open, push to stack
-      if (open[arr[i]]) {
-          stack.push(arr[i]);
-      } else {
-          // if close, check to see if most recent open is matching paren
-          if (close[arr[i]] === stack[stack.length -1]) {
-              // if matching delete from stack
-              stack.length --;
-          } else {
-              // if not matching return false
-              return false;
-          }
-      }
-  }
+    // IDEA: iterate through the string and use stack to make sure that a closing paren is        matched to the closest open paren
+    // make stack arr
+    let stack = []
+    
+    // define open/close obj
+    let open = {
+        '(': ')',
+        '[': ']',
+        '{': '}',
+    };
+    let close = {
+        ')': '(',
+        ']': '[',
+        '}': '{',
+    };
+    
+    // iterate throug s
+    for (let i = 0; i < s.length; i++) {
+        // if open, push to stack
+        if (open[s[i]]) {
+            stack.push(s[i]);
+        } else {
+            // if close, check to see if most recent open is matching paren
+            if (close[s[i]] === stack[stack.length -1]) {
+                // if matching delete from stack
+                stack.pop();
+            } else {
+                // if not matching return false
+                return false;
+            }
+        }
+    }
 
-  if (stack.length === 0) {
-      // if array.length is 0
-      // return true
-      return true
-  }
-  
-  return false
+    if (stack.length === 0) {
+        // if array.length is 0
+        // return true
+        return true
+    }
+    
+    return false
 }
