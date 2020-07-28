8b8768d4f84be74f843a66a5ac1fa769bdd5d85b
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-07-28 12:04:10 -0700

    queue approach

--- remove-element.js
+++ remove-element.js
@@ -4,6 +4,29 @@
 
 //The order of elements can be changed. It doesn't matter what you leave beyond the new length.
 
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
 var removeElement = function(nums, val) {
-  // iterate through the array. keep keep track of index. when i find a value i put it in the queue for so i can replace a non val 
+  // iterate through the array. keep keep track of index. when i find a value i put it in the queue for so i can replace a non val
+   
+   // create index tracker
+   // create obj to store dub
+   // ccreate key tracker to store index of duplicate
+   // create queue tracker
+   
+   // iterate through the array
+       // if not equal val
+           // if there is a queue
+               // change number at the queue to this number
+               // iterate the queue
+           // increment index tracker
+       // else its equal val
+           // we store the index of this val in queue
+           
+   
+   // set array length = index to slice off the rest
+   // return index tracker
 };
