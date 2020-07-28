e33b1668cd38a8ad3bffd05996abf843e98cea74
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-07-28 10:12:32 -0700

    finished

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -4,16 +4,28 @@
 
 var removeDuplicates = function(nums) {
   // overall plan is to loop through array and save index. when we find the first number that is unique we put it at the index and increment the inde.
-  // if empty return 0
+  // empty = true
+  let empty = true;
   // have an index = 1
+  let index = 1;
   // current = num[0]
+  let curr = nums[0];
   
   // loop through nums start at index 1
+  for (let i = 0; i < nums.length; i++) {
+      // set empty = false
+      empty = false;
       // if num not equal to current
+      if (nums[i] != curr) {
           // set current to be this num
+          curr = nums[i];
           // set num @ index to be current
+          nums[index] = curr;
           // increment index
-      
-  
-  //return index
+          index++;
+      }
+  }
+  nums.length = index;
+  //if not empty return index else return 0
+  empty ? 0 : index;
 };
