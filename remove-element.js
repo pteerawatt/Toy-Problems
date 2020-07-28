6fb5e666686996af323d8045741cbd383dc8ef98
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-07-28 11:40:02 -0700

    init

---
+++ remove-element.js
@@ -0,0 +1,9 @@
+// Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+//Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+//The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+var removeElement = function(nums, val) {
+  // iterate through the array. keep keep track of index. when i find a value i put it in the queue for so i can replace a non val 
+};
