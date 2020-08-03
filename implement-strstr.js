4ad1f688d9f5f3a55b9aea245111ea484356947b
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-08-03 11:59:42 -0700

    some progress

--- implement-strstr.js
+++ implement-strstr.js
@@ -6,18 +6,41 @@ var strStr = function(haystack, needle) {
   // main idea is we interate over haystack and we check to see if the following elements align with needle. we do this by recursively calling haystack and decreasing length
   
   //edge cases
+  if (needle = '') {
+      return -1;
+  }
   // current index
+  let currIndex = 0;
   // match index
+  let matchIndex = false;
   // recursive function. takes haystack
-  // increase curr index
+  let iterate = (hay, ned) => {
+      // increase curr index
+      currIndex++;
   // basecase: if needle is empty, we found a match
-      // return match index
+      if (ned === '') {
+          // return match index
+          return matchIndex;
+      } else if (hay === '') {
   // basecase: if haystack is empty
           // this means we finished the whole haystack and no match, return -1
+          return -1;
+      }
       // iterate haystack: compare first character of haystack and needle
-          // set index to 
+      if (hay[0] === ned[0]) {
           // if elements matches recurse(haystack -1, needle -1)
               // if match index is false, set match index to current index
+          if (matchIndex === false) {
+              matchIndex = currIndex;
+          }
+          iterate(hay.substring(1), ned.substring(1));
+      } else {
           // else if not match, recuse(haystack -1, needle)
               // set match index to false
+          matchIndex = false;
+          iterate(hay.substring(1), needle)
+      }
+  }
+  
+  return iterate(haystack, needle);
 };
