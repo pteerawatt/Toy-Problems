e959ecdf093b6eec85726873084987297eb08c13
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-08-12 17:15:21 -0700

    init

---
+++ running_sum.js
@@ -0,0 +1,9 @@
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+// Return the running sum of nums.
+
+// example: nums = [1, 2, 3, 4]
+// output: [1, 3, 6, 10]
+
+var runningSum = function(nums) {
+  
+};
