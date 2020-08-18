3035054e0c972f5f5c5c7d0e06ae5ff5bb1eb41e
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-08-18 13:36:27 -0700

    brut way

---
+++ rotate_array.js
@@ -0,0 +1,9 @@
+// Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+// BRUTE FORCE
+var rotate = function(nums, k) {
+  // loop through array starting from the index that is (length - k)
+  // push index to new array
+  // loop through array again till its before (length - k)
+  // return array
+};
