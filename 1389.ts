3d0acfee61b24a88faf49573e686a5e5ca8d5049
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-11-04 14:01:04 -0800

    implemented targeted array

---
+++ 1389.ts
@@ -0,0 +1,5 @@
+function createTargetArray(nums: number[], index: number[]): number[] {
+  let result: number[] = [];
+  for (let i = 0; i < nums.length; i++) result.splice(index[i], 0, nums[i]);
+  return result;
+};
