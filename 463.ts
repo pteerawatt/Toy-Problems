85223d0784e6bc19856eedf5fe1e87568bb72330
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-11-17 13:47:21 -0800

    almost have solution. need to find a way to set up a condition when we are outside of the matrix.

--- 463.ts
+++ 463.ts
@@ -3,5 +3,17 @@
         // if we hit 1, we look left, top, right, bottom
         // if its water, increment count
 function islandPerimeter(grid: number[][]): number {
+    let count = 0;
     
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] === 1) {
+                if (!grid[i][j - 1]) count++;
+                if (!grid[i - 1][j]) count++;
+                if (!grid[i][j + 1]) count++;
+                if (!grid[i + 1][j]) count++;
+            }
+        }
+    }
+    return count;
 };
