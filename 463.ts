dfbb5eebf7740e4e1b2adc9604eeb2a9359f1305
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-11-17 13:34:46 -0800

    init

---
+++ 463.ts
@@ -0,0 +1,7 @@
+// idea: we have to find the parameters of the island.
+  // loop through the matrix.
+        // if we hit 1, we look left, top, right, bottom
+        // if its water, increment count
+function islandPerimeter(grid: number[][]): number {
+    
+};
