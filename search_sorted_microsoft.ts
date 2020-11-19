a2eb30338c797433b4d64c4a8cd1c7f8574e2120
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-11-18 16:42:40 -0800

    sudo code

---
+++ search_sorted_microsoft.ts
@@ -0,0 +1,7 @@
+// write a function that searches for a number in a sorted array
+
+let search = (): number => {
+  // pick a middle number
+  // if target is middle, return index
+  // if target is more than middle, recurse rightside. else recurse leftside
+}
