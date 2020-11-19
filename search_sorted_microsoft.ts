02e53682264ed9ef0489c4b85bb1fa073f8360e8
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-11-19 14:36:17 -0800

    iterative solution using sliding window

--- search_sorted_microsoft.ts
+++ search_sorted_microsoft.ts
@@ -12,3 +12,29 @@ function search(arr: number[], target: number, left: number = 0, right: number =
 let test = [2, 5, 3, 9];
 
 console.log(search(test, 2))
+
+
+// iterative solution using sliding window
+
+function search(arr: number[], target: number): number {
+  // keep track of mid, left, right
+  let mid = Math.floor(arr.length/2);
+  let left = 0;
+  let right = arr.length -1;
+  // while left > right
+      // check if mid is target
+      // if target > mid, we move the window to the right
+      // else move window to the left
+  while (left <= right) {
+      if (arr[mid] === target) return mid;
+      if (arr[mid] < target) {
+          left = mid + 1;
+          mid = Math.floor((left + right)/2);
+      } else {
+          right = mid - 1;
+          mid = Math.floor((left + right)/2);
+      }
+  }
+  // return -1
+  return -1;
+};
