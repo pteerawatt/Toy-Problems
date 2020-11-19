12656cbfe55e0cd2aa914786425b6e3f7ee93cb6
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-11-18 18:42:23 -0800

    changed the way right is passed when target < arr[mid]. this is now working solution

--- search_sorted_microsoft.ts
+++ search_sorted_microsoft.ts
@@ -1,16 +1,13 @@
 // write a function that searches for a number in a sorted array
 
-let search = (arr: number[], target: number, index: number = 0): number => {
-  // pick a middle number
-  let mid = Math.floor(arr.length/2);
-  // if target is middle, return index
+function search(arr: number[], target: number, left: number = 0, right: number = arr.length - 1): number | undefined{
+  let mid = Math.floor((left + right)/2);
   if (arr[mid] === target) return mid;
-  // if arr.length = 1 && arr[0] is not target, return -1
-  if (arr.length === 1 && arr[0] !== target) return -1;
+  if (left >= right) return -1
   // if target is more than middle, recurse rightside. else recurse leftside
-  if (target > arr[mid]) return search(arr.slice(mid + 1), target, mid);
-  else return search(arr.slice(0, mid), target, mid);
-}
+  if (target > arr[mid]) return search(arr, target, mid + 1, right);
+  if (target < arr[mid]) return search(arr, target, left, mid - 1);
+};
 
 let test = [2, 5, 3, 9];
 
