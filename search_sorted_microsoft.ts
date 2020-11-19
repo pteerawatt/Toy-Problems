6d2d12dff63f8b489a141e041266f9d350f784b2
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-11-18 17:03:36 -0800

    almost working. need to find a way to passover the original index

--- search_sorted_microsoft.ts
+++ search_sorted_microsoft.ts
@@ -1,7 +1,17 @@
 // write a function that searches for a number in a sorted array
 
-let search = (): number => {
+let search = (arr: number[], target: number, index: number = 0): number => {
   // pick a middle number
+  let mid = Math.floor(arr.length/2);
   // if target is middle, return index
+  if (arr[mid] === target) return mid;
+  // if arr.length = 1 && arr[0] is not target, return -1
+  if (arr.length === 1 && arr[0] !== target) return -1;
   // if target is more than middle, recurse rightside. else recurse leftside
+  if (target > arr[mid]) return search(arr.slice(mid + 1), target, mid);
+  else return search(arr.slice(0, mid), target, mid);
 }
+
+let test = [2, 5, 3, 9];
+
+console.log(search(test, 2))
