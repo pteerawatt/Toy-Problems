0c9684c84249f86e70bf2ea6b69916337a1a766d
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-12-01 13:23:49 -0800

    sudo code

--- 1025.ts
+++ 1025.ts
@@ -4,7 +4,7 @@
 // when a player choose a number, replace N with N - x
 // return true if player 1 wins
 
-
+// idea: if a player always choose one. every even number will always lead to first player win. so all we have to do is retrun true if N is even.
 
 const divisorGame = (N: number): boolean => {
 
