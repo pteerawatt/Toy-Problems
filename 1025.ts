892479c322fae140f9ff3b1b69fa0aa26f37ae00
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-12-01 13:22:18 -0800

    init

---
+++ 1025.ts
@@ -0,0 +1,11 @@
+// write a function that determins if first player will win the game
+// start with N number
+// a player takes turn to choose a number x such that 0 < x < N and N % x === 0
+// when a player choose a number, replace N with N - x
+// return true if player 1 wins
+
+
+
+const divisorGame = (N: number): boolean => {
+
+}
