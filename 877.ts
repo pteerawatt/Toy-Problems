25177345f022fd6a963db5a73bda7189fa30904c
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-12-09 10:34:28 -0800

    init

---
+++ 877.ts
@@ -0,0 +1,7 @@
+// write a function to determine if first player wins
+
+// the game is an array of numbers, where player can pick from first or last of the array. there is no tie
+
+const stoneGame = (piles: number[]): boolean => {
+
+}
