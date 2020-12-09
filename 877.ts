e4451de21756323c05a4a3110c30e6c2b35b2c9e
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-12-09 11:17:34 -0800

    algo always picks the biggest number first

--- 877.ts
+++ 877.ts
@@ -2,6 +2,33 @@
 
 // the game is an array of numbers, where player can pick from first or last of the array. there is no tie
 
+// this algo always pick the first biggest number first
 const stoneGame = (piles: number[]): boolean => {
+  let fTotal = 0;
+  let sTotal = 0;
+  let fpt = true;
 
+  let left = 0;
+  let right = piles.length - 1;
+
+  while (left <= right) {
+      let max;
+      if (piles[left] > piles[right]) {
+          max = piles[left];
+          left++;
+      } else {
+          max = piles[right];
+          right--;
+      }
+      if (fpt) {
+          fTotal += max;
+          fpt = false;
+      } else {
+          sTotal += max;
+          fpt = true;
+      }
+      console.log(fTotal, sTotal)
+  }
+  
+  return fTotal > sTotal
 }
