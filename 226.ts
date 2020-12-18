24f8321243af8c5d9909376d0feb552bf39730df
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-12-18 11:46:45 -0800

    sudo code

--- 226.ts
+++ 226.ts
@@ -9,6 +9,11 @@ class TreeNode {
     this.right = (right === undefined ? null : right);
   }
 }
+
+// idea: use DFS to traverse the tree and reverse the values
 const invertTree = (root: TreeNode | null): TreeNode | null => {
-  return null;
+  // base case is if root is null, return
+  // save the left and save the right, then switch the values
+  // recuse left side and recurse right side
+  return root;
 }
