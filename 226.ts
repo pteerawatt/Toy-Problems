fe6d6de4b101a93de80f9cd871222ee87d24b3af
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-12-18 11:37:27 -0800

    init

---
+++ 226.ts
@@ -0,0 +1,14 @@
+// invert binary tree
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = (val === undefined ? 0 : val);
+    this.left = (left === undefined ? null : left);
+    this.right = (right === undefined ? null : right);
+  }
+}
+const invertTree = (root: TreeNode | null): TreeNode | null => {
+  return null;
+}
