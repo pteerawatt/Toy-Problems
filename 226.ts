96ab3a223491ed888771c6b466dd9df43c825701
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-12-18 11:50:58 -0800

    implemented invert tree using DFS

--- 226.ts
+++ 226.ts
@@ -13,7 +13,14 @@ class TreeNode {
 // idea: use DFS to traverse the tree and reverse the values
 const invertTree = (root: TreeNode | null): TreeNode | null => {
   // base case is if root is null, return
+  if (!root) return null;
   // save the left and save the right, then switch the values
+  let left = root.left;
+  root.left = root.right;
+  root.right = left;
   // recuse left side and recurse right side
+  invertTree(root.left);
+  invertTree(root.right);
+
   return root;
 }
