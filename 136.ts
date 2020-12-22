9555c1d9dd09cbfb408ec0c6f7680eed3251aa6b
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-12-21 16:04:06 -0800

    init

---
+++ 136.ts
@@ -0,0 +1,5 @@
+// write a function that takes an array of nums of elements that repeat twice. all but one of the elements only occurs once. return this element
+
+function singleNumber(nums: number[]): number {
+
+};
