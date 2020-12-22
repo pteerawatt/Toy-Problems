f3c3de3b3f46a04d2d6eb707c2d551db3d11fc02
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-12-21 16:13:44 -0800

    working solution in ~O(n). I believe there is a way to improve to constant time

--- 136.ts
+++ 136.ts
@@ -1,5 +1,16 @@
 // write a function that takes an array of nums of elements that repeat twice. all but one of the elements only occurs once. return this element
 
 function singleNumber(nums: number[]): number {
-
+  let memo = {};
+  for (let e of nums) {
+    if (memo[e] || memo[e] === 0) {
+      delete memo[e];
+    } else {
+      memo[e] = e;
+    }
+  }
+  console.log(memo)
+  for (let key in memo) {
+    return memo[key];
+  }
 };
