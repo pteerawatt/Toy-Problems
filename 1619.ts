a62abeb3a776fe123ddd957310d9982f912bbeb4
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-12-22 14:08:19 -0800

    returns the mean of the array after remove the 5% index element that is larges/smallest

---
+++ 1619.ts
@@ -0,0 +1,20 @@
+// given an arr, return the saverage of the elements without the smallest and largest value that has an index that is 5%
+
+// idea: loop over the array and find the sum. meanwhile we keep track of 5% index and the min/max value. return average.
+
+function trimMean(arr: number[]): number {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (i === 20) console.log('here')
+    sum += arr[i];
+    if (i % 5 === 0) {
+      min = Math.min(min, arr[i]);
+      max = Math.max(max, arr[i]);
+    }
+  }
+  return (sum - min - max) / (arr.length - 2);
+};
+
