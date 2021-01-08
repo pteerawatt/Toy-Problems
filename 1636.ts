0624c9f4d8b8670b2c693669b749b9e570b40cb7
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2021-01-08 11:09:53 -0800

    implemented frequency storage

--- 1636.ts
+++ 1636.ts
@@ -3,6 +3,24 @@
 // sort an array by the frequency of an element. the element with the most frequency is last. 
 // if there is an element with the same frequency, sort them in decending order
 
-function frequencySort(nums: number[]): number[] {
+// idea: loop through array and store frequency. (minimum heap)
+  // using minimum heap. if the frequency is the same we put the greater value first
+// use priority queue to generate a new array according to the frequency.
+  // if they are the same sort by decending.
 
+function frequencySort(nums: number[]): number[] {
+  let frequency = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (frequency[nums[i]]) frequency[nums[i]]++
+    else frequency[nums[i]] = 1;
+  }
+  console.log(frequency)
+  return [1];
 };
+
+class minHeap {
+  heap: number[];
+  constructor() {
+    this.heap = [];
+  }
+}
