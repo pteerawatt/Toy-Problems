6393289f614b4a68ce8ee16d44f6fa7a48bb7365
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2021-01-08 10:46:36 -0800

    init

---
+++ 1636.ts
@@ -0,0 +1,8 @@
+// sort array by increasing frequency
+
+// sort an array by the frequency of an element. the element with the most frequency is last. 
+// if there is an element with the same frequency, sort them in decending order
+
+function frequencySort(nums: number[]): number[] {
+
+};
