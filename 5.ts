55d13c55307da7e21349dd5a67f11c4e96831da2
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2021-01-26 16:30:37 -0800

    improved time by checking mirror image from the middle.

--- 5.ts
+++ 5.ts
@@ -20,3 +20,50 @@ function longestPalindrome(s: string): string {
 };
 
 
+// expanding widow method
+
+// the idea is palindrome string are mirrors from the middle. so we have one loop iterating through the array and helper functions to check that the surrounding elements to the left and right are the same. if they are the same it is palindromic
+
+function longestPalindromeMirror(s: string): string {
+  let maxString = "";
+  let string1;
+  let string2;
+  for (let i = 0; i < s.length; i++) {
+      string1 = mirrorWithMid(i, s);
+      string2 = mirrorWOMid(i, s);
+      maxString = maxString.length < string1.length ? string1 : maxString;
+      maxString = maxString.length < string2.length ? string2 : maxString;
+  }
+  return maxString;
+};
+
+let mirrorWithMid = (mid: number, s: string): string => {
+  let left = mid - 1;
+  let right = mid + 1;
+  let max = "";
+  
+  while (left >= 0 && right < s.length) {
+      if (s[left] === s[right]) {
+          max = s.slice(left, right + 1);
+          left--;
+          right++;
+      }
+      else break;
+  }
+  return max === "" ? s[mid] : max;
+}
+
+let mirrorWOMid = (left: number, s: string): string => {
+  let right = left + 1;
+  let max = "";
+  
+  while (left >= 0 && right < s.length) {
+      if (s[left] === s[right]) {
+          max = s.slice(left, right + 1);
+          left--;
+          right++;
+      }
+      else break;
+  }
+  return max === "" ? s[left] : max;
+}
