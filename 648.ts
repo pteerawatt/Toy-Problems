6b2dcabd5698741f10e0d4dc617f6413ed53f3e1
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2021-02-03 15:49:52 -0800

    implemented trie solution

--- 648.ts
+++ 648.ts
@@ -6,8 +6,11 @@
   // each node has a length count.
 // iterate over the sentence and replace words
 
+
+
+
 // this is brute force way of doing it
-// faster than 25% of solutions
+// no improvement in time complexity
 function replaceWords(dictionary: string[], sentence: string): string {
   return sentence.split(" ").map((word) => {
       let replacement = null
@@ -22,3 +25,63 @@ function replaceWords(dictionary: string[], sentence: string): string {
       return replacement ? replacement : word;
   }).join(" ")
 };
+
+
+// using Trie
+// barely improved in time complexity now at 25%
+
+function replaceWords(dictionary: string[], sentence: string): string {
+  let trie = new Trie();
+  dictionary.forEach(word => {
+      trie.insert(word);
+  })
+  
+  return sentence.split(" ").map(word => {
+      let searchResult = trie.search(word);
+      return searchResult ? searchResult : word;
+  }).join(" ");
+};
+
+class Trie {
+  root: TrieNode;
+  constructor() {
+      this.root = new TrieNode("")
+  }
+  
+  insert(word): void {
+      // check current node if it already exist
+          // if not add a new node
+      let currentNode = this.root;
+      for (let i = 0; i < word.length; i++) {
+          if (!currentNode.children[word[i]]) {
+              let newNode = new TrieNode(word[i]);
+              currentNode.children[word[i]] = newNode;
+          }
+          currentNode = currentNode.children[word[i]];
+          if (i === word.length - 1) currentNode.end = true;
+      }
+  }
+  
+  search(word): string | null {
+      let currentNode = this.root;
+      let result = ""
+      for (let ch of word) {
+          if (!currentNode.children[ch]) break;
+          if (currentNode.children[ch].end) return result + ch;
+          result += ch
+          currentNode = currentNode.children[ch];
+      }
+      return null
+  }
+}
+
+class TrieNode {
+  val: string;
+  end: boolean;
+  children: object;
+  constructor(val) {
+      this.val = val;
+      this.children = {}
+      this.end = false;
+  }
+}
