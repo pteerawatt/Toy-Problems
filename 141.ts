5a12f33182c62d5df299febe6455c443f004389d
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2021-02-08 20:30:22 -0800

    implemented a solution using two pointers moving at different speed.

--- 141.ts
+++ 141.ts
@@ -31,3 +31,18 @@ function BAD_hasCycle(head: ListNode | null): boolean {
   }
   return false
 };
+
+// method 2: using 2 runners running at different speed. if the list circles, eventually the runner will be at the same spot
+
+function hasCycle(head: ListNode | null): boolean {
+  let runner1 = head;
+  let runner2 = head;
+  while(runner1 && runner2) {
+      runner1 = runner1.next;
+      runner2 = runner2.next;
+      if (runner2) runner2 = runner2.next
+      else return false;
+      if (runner1 === runner2) return true;
+  }
+  return false;
+};
